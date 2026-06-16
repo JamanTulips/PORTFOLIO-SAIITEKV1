de5a1e40e868002b55b485c688871585fa6d7d93
JamanTulips <jamanbata@tulipstechnologies.com>
2026-06-16 14:09:46 +0545

    Improvement: Hotfixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,12 @@ export const metadata = createPageMetadata({
   path: "/",
   absoluteTitle: true,
 });
-// import AboutSection from "@/components/AboutSection";
 import HealthcareInnovation from "@/components/HealthcareInnovation";
-// import ServicesSection from "@/components/ServicesSection";
 import HealthcareServices from "@/components/HealthcareServices";
-// import ProjectsSection from "@/components/ProjectsSection";
 import ClinicalNetwork from "@/components/ClinicalNetwork";
 import ContactSection from "@/components/ContactSection";
-import BlogSection from "@/components/BlogSection";
 import GalleryCarousel from "@/components/GalleryCarousel";
 import PartnersStandards from "@/components/PartnersStandards";
-import TeamSection from "@/components/TeamSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
 import styles from "./page.module.css";
 
 export default function Home() {
